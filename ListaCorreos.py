fcaa1d4d8b95ba1b7bba02e4f9673b0df5b7e4da
PrintRonald <madariaga492@gmail.com>
2025-04-29 11:15:04 -0400

    Lista correos de profe hugo
    Udpate lista de curso para profe hugo

--- ListaCorreos.py
+++ ListaCorreos.py
@@ -22,4 +22,14 @@ CAROLINADIAZ22@HOTMAIL.COM, giovi_noemi@hotmail.com, karinasofia15@outlook.com, 
 
 """
 
+"""
+cfmfilo@gmail.com, neve.jimenez.vilches@gmail.com, info.torosalinas@gmail.com, vane.silvaparra@gmail.com, inesvasquez1@gmail.com,
+ sotelonicole@gmail.com, danitajaksic@gmail.com, mahuen@gmail.com, tamaruz2010@gmail.com, dilujo1076@gmail.com,
+   saavedramarjorie39@gmail.com, lornapuguita@gmail.com, alemancilla2006@gmail.com, isaabemargarita@gmail.com, jovitajoyas@gmail.com,
+     carolinadunstan@hotmail.com, lissette.cortes.araya@gmail.com, marielamilena1973@gmail.com, dangrafics@gmail.com,
+       dangrafics@gmail.com, juliainocenciob@gmail.com, daa.vgodoy@gmail.com, sangopa1970@gmail.com, almendragr@gmail.com,
+         ivo.silva.carvajal12@gmail.com, brandorita1980@gmail.com, milenka.azagra@gmail.com, sep.so.pablo@gmail.com, 
+         pau.diaz.vargas@gmail.com, dponce.ag@gmail.com, leorodriguez60@gmail.com, gabrieladunstan@hotmail.com,
+           cristian.daniel.salinas@gmail.com, ruthgon@gmail.com
 
+"""
