183598360d8af7f3cbe591107905bd58d2b6570b
PrintRonald <madariaga492@gmail.com>
2025-05-15 11:19:45 -0400

    Lista correo Hugo
    Mandar correos masivos para el profe

--- coincide.py
+++ coincide.py
@@ -12,5 +12,13 @@ print('no coincide')
 
 """
 Correos 5BB
-'CAROLINADIAZ22@HOTMAIL.COM', 'giovi_noemi@hotmail.com', 'karinasofia15@outlook.com', 'vane.silvaparra@gmail.com', 'Cherieleclerc@gmail.com', 'mgch84@gmail.com', 'danitzacarvajal@gmail.com', 'andrus.olivarez@gmail.com', 'stephanibb@gmail.com', 'lci_tj@hotmail.com', 'lorevera28@gmail.com', 'danielasotozamora@gmail.com', 'africacortez@icloud.com', 'mbaezamontoya@gmail.com', 'MESTAYTOLEDO@HOTMAIL.COM', 'fdelcei1985@gmail.com', 'carolina.valdes.villegas@gmail.com', 'sicher.eirl@gmail.com', 'karol.velastegui.kv@gmail.com', 'A.ARDILESE@GMAIL.COM', 'rodolfo.quezada.s@gmail.com', 'camilatobar661@gmail.com', 'camipinedav@gmail.com', 'j.barriossaavedra@gmail.com', 'ppeiranogatica@gmail.com', 'fsaconstrucciones@gmail.com', 'nicoledesidelguerra14@outlook.com', 'MSMARY624@GMAIL.COM', 'ibaniapablaza87@gmail.com', 'gonzalo16744@yahoo.es', 'alvarado.susana@hotmail.com', 'MARIA_ELENA_VERA@HOTMAIL.COM', 'micavifi@gmail.com', 'olgasilva1523@gmail.com', 'lorenaavilapizarro@gmail.com', 'alejandro.terapiasistemica@gmail.com', 'ruthgon@gmail.com'
+CAROLINADIAZ22@HOTMAIL.COM, giovi_noemi@hotmail.com, karinasofia15@outlook.com, vane.silvaparra@gmail.com, Cherieleclerc@gmail.com,
+mgch84@gmail.com, danitzacarvajal@gmail.com, andrus.olivarez@gmail.com, stephanibb@gmail.com, lci_tj@hotmail.com,
+lorevera28@gmail.com, danielasotozamora@gmail.com, africacortez@icloud.com, mbaezamontoya@gmail.com,
+MESTAYTOLEDO@HOTMAIL.COM, fdelcei1985@gmail.com, carolina.valdes.villegas@gmail.com, sicher.eirl@gmail.com,
+karol.velastegui.kv@gmail.com, A.ARDILESE@GMAIL.COM, rodolfo.quezada.s@gmail.com, camilatobar661@gmail.com,
+camipinedav@gmail.com, j.barriossaavedra@gmail.com, ppeiranogatica@gmail.com, fsaconstrucciones@gmail.com,
+nicoledesidelguerra14@outlook.com, MSMARY624@GMAIL.COM, ibaniapablaza87@gmail.com, gonzalo16744@yahoo.es,
+alvarado.susana@hotmail.com, MARIA_ELENA_VERA@HOTMAIL.COM, micavifi@gmail.com, olgasilva1523@gmail.com,
+lorenaavilapizarro@gmail.com, alejandro.terapiasistemica@gmail.com, ruthgon@gmail.com
 """
